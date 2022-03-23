89ac6b2be5fea1cfc187910fb42871b50be402ed
Sayam Gautam <sayamgautam1@gmail.com>
2022-03-24 07:44:11 +1030

    refractored to add one working station calling all function

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 //provide alphabets , number and symbols defined inside an object
 
@@ -9,17 +9,36 @@ const passwordOptions = {
   number: "0123456789",
   symbol: "!@#$%^&*()_+~\\`|}{[]:;?><,./-=",
 };
-var isUpperCase;
-var isLowerCase;
-var isNumber;
-var isSymbol;
-var passwordLength;
+// var isUpperCase = true;
+// var isLowerCase;
+// var isNumber;
+// var isSymbol;
+// var passwordLength = 0;
 var password;
-var passwordReturned;
+var passwordReturned = "";
 
 // Write password to the #password input
 function writePassword() {
-  selectCharacters();
+  //function to get prefences from the user
+  const preferences = getPasswordPreferences();
+
+  // check the preferences via another function
+  const prefsAreValid = validatePrefrences(preferences);
+
+  //get length of the password
+
+  const length = passwordLength(prefsAreValid);
+
+  // check length of the character
+
+  const lengthIsValid = validateLength(length);
+
+  // generate password if everything is valid
+
+  if (lengthIsValid === true) {
+    password = generatePassword(preferences, length);
+  }
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -27,40 +46,55 @@ function writePassword() {
 
 // create a function to enter all the characters of the password
 
-function selectCharacters() {
+function getPasswordPreferences() {
   //add confim to get input from the user on their password type//
-  isUpperCase = confirm(
+  var isUpperCase = confirm(
     "Do you want yout passwword to have uppercase letters?"
   );
-  isLowerCase = confirm(
+  var isLowerCase = confirm(
     "Do you want yout passwword to have lowercase letters?"
   );
-  isNumber = confirm("Do you want yout passwword to have numberical value?");
-  isSymbol = confirm("Do you want yout passwword to have symbols?");
-  checkCharacter();
+  var isNumber = confirm(
+    "Do you want yout passwword to have numberical value?"
+  );
+  var isSymbol = confirm("Do you want yout passwword to have symbols?");
+
+  return { isUpperCase, isLowerCase, isNumber, isSymbol };
 }
 
 // create a function to check if all the characters are selected or not
-function checkCharacter() {
+function validatePrefrences() {
   if (!isUpperCase && !isLowerCase && !isNumber && !isSymbol) {
     alert("please select at least on type of character..");
-    selectCharacters();
+    return false;
   } else {
-    passwordLength = prompt(
-      "How long do you want you password to be [8>length<128] ?"
-    );
-  }
-
-  if (passwordLength > 8 && passwordLength < 128) {
-    password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-  } else {
-    alert("please enter a valid password length [8>length<128], ");
-    checkCharacter();
+    return true;
   }
 }
+
+//create a function to get the password length
+function passwordLength() {
+  if (prefsAreValid) {
+    var passwordLengthUser = prompt(
+      "How long do you want you password to be [8>length<128] ?"
+    );
+    return passwordLengthUser;
+  } else {
+    alert("Please enter at least one character");
+    writePassword();
+  }
+}
+
+//create a function to return if pasword length is valid or not
+function validateLength() {
+  if (length > 8 && length < 128) {
+    return true;
+  } else {
+    alert("please enter a valid password length [8>length<128], ");
+    passwordLength();
+  }
+}
+
 //add function to geenrate password
 
 function generatePassword() {
@@ -82,11 +116,11 @@ function generatePassword() {
     var symbolString = passwordOptions.symbol.split("");
     passwordArray = passwordArray.concat(symbolString);
   }
-  for (i = 0; i < passwordLength; i++) {
+  for (i = 0; i < length; i++) {
     passwordReturned +=
       passwordArray[Math.floor(Math.random() * passwordArray.length)];
   }
-  console.log(passwordReturned);
+  return passwordReturned;
 }
 
 // Add event listener to generate button
