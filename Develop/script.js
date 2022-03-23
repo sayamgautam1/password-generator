d0701b5c3abd860769c4584cdc9bc4d0b91c747c
Sayam Gautam <sayamgautam1@gmail.com>
2022-03-23 18:09:24 +1030

    add function to input character and function to check and obtain length for the password

--- Develop/script.js
+++ Develop/script.js
@@ -9,32 +9,15 @@ const passwordOptions = {
   number: "0123456789",
   symbol: "!@#$%^&*()_+~\\`|}{[]:;?><,./-=",
 };
+var isUpperCase;
+var isLowerCase;
+var isNumber;
+var isSymbol;
+var passwordLength;
 
 // Write password to the #password input
 function writePassword() {
-  //add confim to get input from the user on their password type//
-  const isUpperCase = confirm(
-    "Do you want yout passwword to have uppercase letters?"
-  );
-  const isLowerCase = confirm(
-    "Do you want yout passwword to have lowercase letters?"
-  );
-  const isNumber = confirm(
-    "Do you want yout passwword to have numberical value?"
-  );
-  const isSymbol = confirm("Do you want yout passwword to have symbols?");
-
-  var passwordLength;
-
-  //check if all the fields are selcted
-  if (!isUpperCase && !isLowerCase && !isNumber && !isSymbol) {
-    alert("please select at least on type of character..");
-    return;
-  } else {
-    passwordLength = prompt(
-      "How long do you want you password to be [8>length<128] ?"
-    );
-  }
+  selectCharacters();
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -42,5 +25,38 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// create a function to enter all the characters of the password
+
+function selectCharacters() {
+  //add confim to get input from the user on their password type//
+  isUpperCase = confirm(
+    "Do you want yout passwword to have uppercase letters?"
+  );
+  isLowerCase = confirm(
+    "Do you want yout passwword to have lowercase letters?"
+  );
+  isNumber = confirm("Do you want yout passwword to have numberical value?");
+  isSymbol = confirm("Do you want yout passwword to have symbols?");
+  checkCharacter();
+}
+
+// create a function to check if all the characters are selected or not
+function checkCharacter() {
+  if (!isUpperCase && !isLowerCase && !isNumber && !isSymbol) {
+    alert("please select at least on type of character..");
+    selectCharacters();
+  } else {
+    passwordLength = prompt(
+      "How long do you want you password to be [8>length<128] ?"
+    );
+  }
+
+  if (passwordLength > 8 && passwordLength < 128) {
+    generatePassword();
+  } else {
+    alert("please enter a valid password length [8>length<128], ");
+    checkCharacter();
+  }
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
